97b640325b35202a4b8605b88e2708bd7b523265
dfralan <80047722+dfralan@users.noreply.github.com>
2023-01-02 15:48:18 -0300

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var opts = {
     referrer: "no-referrer"
 };
 
-var fullFeedbackurl = "https://docs.google.com/forms/d/e/1FAIpQLSegCYzLpH4bIgQfYgmj8-Hk5T6qDCTHrdmg57Qb5EsMePlYBw/viewform?usp=pp_url&entry.1948324434=blabla";
+var fullFeedbackurl = "https://docs.google.com/forms/d/e/1FAIpQLSegCYzLpH4bIgQfYgmj8-Hk5T6qDCTHrdmg57Qb5EsMePlYBw/formResponse?usp=pp_url&entry.1948324434=blabla";
 
 window.onload = function() {
     fetch(fullFeedbackurl, opts)
