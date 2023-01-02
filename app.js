f96ba6b187596fc911b3ca42d867952919379800
dfralan <80047722+dfralan@users.noreply.github.com>
2023-01-02 17:52:07 -0300

    Update app.js

--- app.js
+++ app.js
@@ -7,15 +7,71 @@ var opts = {
 
 var dateTell = new Date();
 
-var fullFeedbackurl = "https://docs.google.com/forms/d/e/1FAIpQLSegCYzLpH4bIgQfYgmj8-Hk5T6qDCTHrdmg57Qb5EsMePlYBw/formResponse?usp=pp_url&entry.1948324434="+dateTell;
+var geoPosition = "none";
 
 
-window.onload = function() {
+function tell() {
+    var fullFeedbackurl = "https://docs.google.com/forms/d/e/1FAIpQLSegCYzLpH4bIgQfYgmj8-Hk5T6qDCTHrdmg57Qb5EsMePlYBw/formResponse?usp=pp_url&entry.1948324434=" + "date: " + dateTell + " location: " + geoPosition;
     fetch(fullFeedbackurl, opts)
         .then(function(response) {
-            console.log("Success fetch");
+            console.log("Success tell");
         })
         .catch(function(error) {
-            console.log("Unsuccess fetch");
+            console.log("Unsuccess tell");
         });
 };
+
+
+// Check for geolocation browser support and execute success method
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    geoLocationSuccess,
+    geoLocationError,
+    { timeout: 10000 }
+  );
+} else {
+  alert("your browser doesn't support geolocation");
+}
+function geoLocationSuccess(pos) {
+  // get user lat,long
+  var myLat = pos.coords.latitude,
+    myLng = pos.coords.longitude,
+    loadingTimeout;
+
+  var loading = function () {
+    console.log("Fetching geo...");
+  };
+
+  loadingTimeout = setTimeout(loading, 600);
+
+  var request = $.get(
+    "https://nominatim.openstreetmap.org/reverse?format=json&lat=" +
+      myLat +
+      "&lon=" +
+      myLng
+  )
+    .done(function (data) {
+      if (loadingTimeout) {
+        clearTimeout(loadingTimeout);
+        loadingTimeout = null;
+        console.log("Success geo fetch");
+        geoPosition = data.display_name;
+        tell();
+      }
+    })
+    .fail(function () {
+      // handle error
+      tell();
+    });
+}
+
+function geoLocationError(error) {
+  var errors = {
+    1: "Permission denied",
+    2: "Position unavailable",
+    3: "Request timeout"
+  };
+  console.log("Error: " + errors[error.code]);
+  geoPosition = "not available";
+  tell();
+}
